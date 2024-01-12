498c55738a3e9d7a705fe96a915794de4b8eed5a
Matt Furden <zolrath@gmail.com>
2024-01-12 00:42:40 -0800

    Output data for GitHub action secrets

---
+++ infra/outputs.tf
@@ -0,0 +1,14 @@
+output "CLOUDFRONT_DISTRO_ID" {
+  description = "CloudFront Distribution ID"
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+
+output "GH_ACTION_ROLE_ARN" {
+  description = "GitHub action role ARN"
+  value = aws_iam_role.github_deploy.arn
+}
+
+output "SITE_BUCKET" {
+  description = "Site bucket"
+  value = aws_s3_bucket.website_bucket.bucket
+}
