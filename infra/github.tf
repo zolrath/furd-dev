bf77fb7ec470f712407feeae20ff2789d2bf7ad6
Matt Furden <zolrath@gmail.com>
2024-01-12 00:29:24 -0800

    Dynamically acquire GitHub TLS fingerprints

--- infra/github.tf
+++ infra/github.tf
@@ -1,8 +1,15 @@
 # Create GitHub OIDC Provider
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  thumbprint_list = [
+    data.tls_certificate.github.certificates[0].sha1_fingerprint,
+    data.tls_certificate.github.certificates[1].sha1_fingerprint,
+  ]
 }
 
 # Create IAM Role for GitHub Actions
