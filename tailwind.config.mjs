678ef961db3658be25197b0d85398d0a9558f1cb
Matt Furden <zolrath@gmail.com>
2024-01-18 15:33:07 -0800

    Add Featured Blog Post

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -78,6 +78,9 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      aspectRatio: {
+        '7/4': '7 / 4',
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography"), require("tailwind-scrollbar")],
