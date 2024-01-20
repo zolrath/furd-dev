ad1514192812ec4d98012945d2d6ba01c55f5716
Matt Furden <zolrath@gmail.com>
2024-01-20 01:54:25 -0800

    Disable MdxComponents headings to use prose

--- src/components/content/MdxComponents.tsx
+++ src/components/content/MdxComponents.tsx
@@ -13,75 +13,75 @@ type Props = {
 }
 
 export const MdxComponents = {
-  h1: ({ className, ...props }: Props) => (
-    <h1
-      className={cn(
-        "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
-        className
-      )}
-      {...props}
-    />
-  ),
-  h2: ({ className, ...props }: Props) => (
-    <h2
-      className={cn(
-        "mt-10 scroll-m-20 pb-1 text-3xl font-semibold tracking-tight first:mt-0",
-        className
-      )}
-      {...props}
-    />
-  ),
-  h3: ({ className, ...props }: Props) => (
-    <h3
-      className={cn(
-        "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
-        className
-      )}
-      {...props}
-    />
-  ),
-  h4: ({ className, ...props }: Props) => (
-    <h4
-      className={cn(
-        "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
-        className
-      )}
-      {...props}
-    />
-  ),
-  h5: ({ className, ...props }: Props) => (
-    <h5
-      className={cn(
-        "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
-        className
-      )}
-      {...props}
-    />
-  ),
-  h6: ({ className, ...props }: Props) => (
-    <h6
-      className={cn(
-        "mt-8 scroll-m-20 text-base font-semibold tracking-tight",
-        className
-      )}
-      {...props}
-    />
-  ),
-  p: ({ className, ...props }: Props) => (
-    <p
-      className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
-      {...props}
-    />
-  ),
-  ul: ({ className, ...props }: Props) => (
-    <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
-  ),
-  ol: ({ className, ...props }: Props) => (
-    <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
-  ),
-  li: ({ className, ...props }: Props) => (
-    <li className={cn("mt-2", className)} {...props} />
-  ),
+  // h1: ({ className, ...props }: Props) => (
+  //   <h1
+  //     className={cn(
+  //       "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
+  //       className
+  //     )}
+  //     {...props}
+  //   />
+  // ),
+  // h2: ({ className, ...props }: Props) => (
+  //   <h2
+  //     className={cn(
+  //       "mt-10 scroll-m-20 pb-1 text-3xl font-semibold tracking-tight",
+  //       className
+  //     )}
+  //     {...props}
+  //   />
+  // ),
+  // h3: ({ className, ...props }: Props) => (
+  //   <h3
+  //     className={cn(
+  //       "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
+  //       className
+  //     )}
+  //     {...props}
+  //   />
+  // ),
+  // h4: ({ className, ...props }: Props) => (
+  //   <h4
+  //     className={cn(
+  //       "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
+  //       className
+  //     )}
+  //     {...props}
+  //   />
+  // ),
+  // h5: ({ className, ...props }: Props) => (
+  //   <h5
+  //     className={cn(
+  //       "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
+  //       className
+  //     )}
+  //     {...props}
+  //   />
+  // ),
+  // h6: ({ className, ...props }: Props) => (
+  //   <h6
+  //     className={cn(
+  //       "mt-8 scroll-m-20 text-base font-semibold tracking-tight",
+  //       className
+  //     )}
+  //     {...props}
+  //   />
+  // ),
+  // p: ({ className, ...props }: Props) => (
+  //   <p
+  //     className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
+  //     {...props}
+  //   />
+  // ),
+  // ul: ({ className, ...props }: Props) => (
+  //   <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+  // ),
+  // ol: ({ className, ...props }: Props) => (
+  //   <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+  // ),
+  // li: ({ className, ...props }: Props) => (
+  //   <li className={cn("mt-2", className)} {...props} />
+  // ),
   blockquote: ({ className, ...props }: Props) => (
     <blockquote
       className={cn(
